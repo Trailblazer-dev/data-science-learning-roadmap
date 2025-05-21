81d3ad0d6a830784a3995bed7c671d62d904e186
Trailblazer-dev <richvictor830@gmail.com>
2025-05-21 12:59:40 +0300

    day 1 done

---
+++ Month_1/Week_1/hello-word.py
@@ -0,0 +1,10 @@
+
+message = "Hello Python world!"
+print(message)
+
+
+message = "Hello Python Crash Course world!"
+print (message)
+
+name = input("Please enter your name: \n")
+print("Hello ",name," Welcome to Python crash Course by Eric Matthes")
