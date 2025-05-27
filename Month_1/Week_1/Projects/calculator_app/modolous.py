c51c4f4540d99982a53e7743bffdb9b0188d3151
Trailblazer-dev <richvictor830@gmail.com>
2025-05-27 23:37:49 +0300

    27th May 2025 done with calculator project

---
+++ Month_1/Week_1/Projects/calculator_app/modolous.py
@@ -0,0 +1,13 @@
+def modulus(a, b):
+    """
+    Returns the modulus of a and b.
+    
+    Parameters:
+    a (int or float): The dividend.
+    b (int or float): The divisor.
+    
+    Returns:
+    int or float: The modulus of a and b.
+    """
+    result = a % b
+    return result
